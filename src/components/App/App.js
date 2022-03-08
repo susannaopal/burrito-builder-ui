c455d5fd3bc1851439a98059603add51f1a5d096
Susanna Carey <susannaopal@gmail.com>
2022-03-08 12:10:32 -0500

    Update orders container to include cards for each order, add mock dummy data from API to test if things being rendered correctly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+// import {getOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
-  constructor(props) {
+  // removing props from the constructor since not usually there (if an issue later then can bring back)
+  constructor() {
     super();
+    this.state = {
+      orders: [
+        {
+          id: 1,
+          name: 'Pat',
+          ingredients: ['beans', 'lettuce', 'carnitas', 'queso fresco', 'jalapeno']
+        }
+      ]
+    }
   }
 
-  componentDidMount() {
-    getOrders()
-      .catch(err => console.error('Error fetching:', err));
-  }
+  // componentDidMount() {
+  //   getOrders()
+  //   .then(data => this.setState({data => orders: data.orders}))
+  //   .catch(err => console.error('Error fetching:', err));
+  // }
 
   render() {
     return (
@@ -21,7 +32,6 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
         <Orders orders={this.state.orders}/>
       </main>
     );
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import './Orders.css';
+import Card from '../../components/Card/Card';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  });
 
+// const Orders = props => {
+//   const orderEls = props.orders.map(order => {
+//     return (
+//       <div className="order">
+//         <h3>{order.name}</h3>
+//         <ul className="ingredient-list">
+//           {order.ingredients.map(ingredient => {
+//             return <li>{ingredient}</li>
+//           })}
+//         </ul>
+//       </div>
+//     )
+//   });
+
+//   return (
+//     <section>
+//       {/* { orderEls.length ? orderEls : <p>No orders yet!</p> } */}
+//     </section>
+//   )
+// }
+
+const Orders = ({orders}) => {
+    const orderEls = orders.map((order, index) => {
+      return (
+        <Card 
+          id={order.id}
+          key={order.id}
+          name={order.name}
+          ingredients={order.ingredients}
+        />
+      )
+    })
   return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
+    <div className='order'>
+      {orderEls}
+    </div>
   )
 }
 
 export default Orders;
+
