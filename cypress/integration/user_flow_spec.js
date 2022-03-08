2d6b70d5ce8bc93a42426dcc95baf7137157f3a1
Susanna Carey <susannaopal@gmail.com>
2022-03-08 14:52:45 -0500

    Add intercept and test with wifi off tfor first test

--- cypress/integration/user_flow_spec.js
+++ cypress/integration/user_flow_spec.js
@@ -1,14 +1,35 @@
 describe('User Flow View', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/orders)', {fixture: "orders.json"})
-    .visit('http://localhost:3001/api/v1/orders')
-})
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders)', {
+      statusCode: 200,
+      body: [{
+        id: 1,
+        name: "Pat",
+        ingredients: [
+        "beans",
+        "lettuce",
+        "carnitas",
+        "queso fresco",
+        "jalapeno"
+        ]     
+    }]
+  })
+    cy.visit('http://localhost:3000/');
+});
+
   it('should be able to visit the url', () => {
     cy.url()
-    .should('eq', 'http://localhost:3001/')
-  })
+      .should('eq', 'http://localhost:3000/')
+  });
 
-})
+  it('should be see a title at the top of the app', () => {
+    cy.get('h1')
+      .contains('Burrito Builder')
+      .should('be.visible')
+  })
+});
+
+
 
 
 // Write tests for the form and overall app user flows. Be sure to intercept the network requests.
