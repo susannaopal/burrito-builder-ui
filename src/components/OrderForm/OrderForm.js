9609d007b6436e4f55f15c8694dd4997a265cf75
Susanna Carey <susannaopal@gmail.com>
2022-03-08 14:15:01 -0500

    Add error handling in case ingredients array is empty

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -23,7 +23,7 @@ class OrderForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    if(this.state.name === '') {
+    if(this.state.name === ''  || this.state.ingredients.length === 0) {
       this.setState({showError: true})
     } else {
     const newName = {
